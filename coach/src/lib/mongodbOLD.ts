f960412e9aca7afc0ad6e21e019ea6606e86fc07
deltagamingch <deltagaming.ch.nik@gmail.com>
2024-11-22 17:32:25 +0100

    feat(db): update mongodb connection, comment out old script for possible future use

---
+++ coach/src/lib/mongodbOLD.ts
@@ -0,0 +1,14 @@
+/*
+const uri: string = process.env.MONGODB_URI as string;
+
+const connectDb = async () => {
+    try {
+        await mongoose.connect(uri);
+        console.log("Successfully connected to MongoDB");
+    } catch (error) {
+        console.error("Failed to connect to MongoDB", error);
+    }
+};
+
+export default connectDb;
+*/

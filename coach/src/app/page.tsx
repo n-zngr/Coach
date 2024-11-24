348231e85017e213fe89db05c6135817f7e5dc6c
deltagamingch <deltagaming.ch.nik@gmail.com>
2024-11-24 14:02:51 +0100

    fix: add suppressHydrationWarning, suppressing client-based error caused by browser extensions

--- coach/src/app/page.tsx
+++ coach/src/app/page.tsx
@@ -1,8 +1,6 @@
-export default async function Home() {
+export default function Home() {
 
     return (
-        <div>
-            <h1>Welcome to the Next.js App</h1>
-        </div>
+        <h1>Welcome to the Next.js App</h1>
     );
 }

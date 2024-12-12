c531abf5b3a1515d2e4f647f7586d73394cfddcc
deltagamingch <deltagaming.ch.nik@gmail.com>
2024-12-12 17:05:53 +0100

    feat(auth)!: use middleware to authenticate user (unclear why middleware does not check documents)

--- middleware.ts
+++ middleware.ts
@@ -1,25 +1,30 @@
-import { NextResponse } from 'next/server';
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { MongoClient, ObjectId } from 'mongodb';
 
-const uri = process.env.MONGODB_URI as string;
-const dbName = 'users';
-const collectionName = 'users';
+const MONGODB_URI = process.env.MONGODB_URI as string;
+const DATABASE_NAME = 'users';
+const COLLECTION_NAME = 'users';
 
 let client: MongoClient | null = null;
 
 async function connectToDatabase() {
     if (!client) {
-        client = new MongoClient(uri);
+        client = new MongoClient(MONGODB_URI);
         await client.connect();
     }
-    return client.db(dbName).collection(collectionName);
+    return client.db(DATABASE_NAME).collection(COLLECTION_NAME);
 }
 
 export async function middleware(req: NextRequest) {
     const userId = req.cookies.get('userId')?.value;
 
     if (!userId) {
+        console.warn('UserId cookie not found. Redirecting to /login.');
+        return NextResponse.redirect(new URL('/login', req.url));
+    }
+
+    if (!ObjectId.isValid(userId)) {
+        console.warn('UserId is not a valid ObjectId. Redirecting to /login.');
         return NextResponse.redirect(new URL('/login', req.url));
     }
 
@@ -28,16 +33,17 @@ export async function middleware(req: NextRequest) {
         const user = await collection.findOne({ _id: new ObjectId(userId) });
 
         if (!user) {
+            console.warn(`User with userId: ${userId} not found in the database. Redirecting to /login.`);
             return NextResponse.redirect(new URL('/login', req.url));
         }
 
         return NextResponse.next();
     } catch (error) {
-        console.error('Error in middleware', error);
+        console.error('Error in middleware: ', error);
         return NextResponse.redirect(new URL('/login', req.url));
     }
 }
 
 export const config = {
     matcher: ['/documents/:path*', '/api/:path*']
-}
+};

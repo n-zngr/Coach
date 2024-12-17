7cac2feb6a697dfaad9121d790a22b3131900af1
deltagamingch <deltagaming.ch.nik@gmail.com>
2024-12-17 12:05:58 +0100

    style(documents): changes to recent files styling

--- src/app/components/RecentFiles.tsx
+++ src/app/components/RecentFiles.tsx
@@ -70,19 +70,17 @@ const RecentFiles: React.FC<ShowRecentProps> = ({ semesterId, subjectId, topicId
             <h2 className="text-xl font-semibold mb-4 text-gray-900 dark:text-white">Recent Files</h2>
             <div className="flex flex-wrap gap-4">
                 {files.map((file) => (
-                    <div key={file._id} className="flex items-center gap-4 p-4 rounded-lg shadow-md bg-white dark:bg-gray-800 dark:shadow-gray-700 hover:shadow-lg transition-shadow">
-                        <img
-                        src="/icon-document.svg"
-                        alt="Document Icon"
-                        className="w-12 h-12"
-                        />
-                    <div>
-                        <p className="font-medium text-gray-900 dark:text-white">{file.filename}</p>
-                        <p className="text-sm text-gray-500 dark:text-gray-400">
-                            {new Date(file.uploadDate).toLocaleDateString()} &middot;{" "}
-                            {(file.length / (1024 * 1024)).toFixed(1)} MB
-                        </p>
-                    </div>
+                    <div key={file._id} className="flex items-center flex-row gap-4 p-4 border border-rounded border-gray-200 rounded-lg bg-white dark:bg-gray-800">
+                        <div className="p-4 rounded-lg bg-gray-200 dark:bg-gray-800">
+                            <img src="/icon-document.svg" alt="Document Icon" className="w-8 h-8"/>
+                        </div>
+                        <div>
+                            <p className="font-medium text-gray-900 dark:text-white">{file.filename}</p>
+                            <p className="text-sm text-gray-500 dark:text-gray-400">
+                                {new Date(file.uploadDate).toLocaleDateString()} &middot;{" "}
+                                {(file.length / (1024 * 1024)).toFixed(1)} MB
+                            </p>
+                        </div>
                     </div>
                 ))}
             </div>

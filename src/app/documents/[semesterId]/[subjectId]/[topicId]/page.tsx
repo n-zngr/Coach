591a5e08994c7d06c8215fc112163d695f9e1537
deltagamingch <deltagaming.ch.nik@gmail.com>
2024-12-17 16:41:48 +0100

    feat(documents): add navigation to all documents pages, move RecentFiles to be displayed above DisplayFIles

--- src/app/documents/[semesterId]/[subjectId]/[topicId]/page.tsx
+++ src/app/documents/[semesterId]/[subjectId]/[topicId]/page.tsx
@@ -6,9 +6,11 @@ import { useParams, useRouter } from "next/navigation";
 import FileDisplay from "@/app/components/DisplayFiles";
 import RecentFiles from "@/app/components/RecentFiles";
 import UploadFile from "@/app/components/UploadFile";
+import Navigation from "@/app/components/Navigation";
 
 const TopicPage = () => {
     const [isLoading, setIsLoading] = useState(true);
+    const [isExpanded, setIsExpanded] = useState(true);
     const router = useRouter();
     const params = useParams();
 
@@ -35,6 +37,10 @@ const TopicPage = () => {
         setIsLoading(false);
     }, []);
 
+    const toggleNavigation = () => {
+        setIsExpanded(!isExpanded);
+    }
+
     if (isLoading) {
         return (
             <div className="container mx-auto p-4">
@@ -44,14 +50,19 @@ const TopicPage = () => {
     }
 
     return (
-        <div className="container mx-auto p-4">
-            <h1 className="text-2xl font-bold mb-4">Topic</h1>
-
-            <UploadFile />
-            <h1 className='text-2xl font-semibold my-4'>Documents</h1>
-            <FileDisplay />
-            <h1 className='text-2xl font-semibold my-4'>Recent Documents</h1>
-            <RecentFiles />
+        <div>
+            <Navigation isExpanded={isExpanded} toggleNavigation={toggleNavigation} />
+            <div className={`flex-1 p-16 transition-all duration-300
+                ${
+                    isExpanded ? "ml-64" : "ml-12"
+                }`}
+                >
+                <h1 className="text-2xl font-bold mb-4">Topic</h1>
+                <UploadFile />
+                <h1 className='text-2xl font-semibold my-4'>Documents</h1>
+                <RecentFiles />
+                <FileDisplay />
+            </div>
         </div>
     );
 };

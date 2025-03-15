3c5d6b6b677239ec7bcfd426e44165e10ac90bbe
n-zngr <deltagaming.ch.nik@gmail.com>
2025-03-15 12:35:22 +0100

    feat(search)*: replace search button with inputRef
    - remove search Button and functionality
    - replace search button with inputRef search through timeout
    - implement search through pressing enter, avoiding inputRef timeout

--- src/app/components/Search/Search.tsx
+++ src/app/components/Search/Search.tsx
@@ -13,15 +13,37 @@ interface SearchProps {
 const Search: React.FC<SearchProps> = ({ onClose, onSearch, searchQuery, searchResults }) => {
     const [query, setQuery] = useState(searchQuery);
     const searchRef = useRef<HTMLDivElement>(null);
+    const inputRef = useRef<HTMLInputElement>(null);
+    const searchTimeout = useRef<NodeJS.Timeout | null>(null);
+    const closeButtonClicked = useRef<boolean>(false);
 
-    const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setQuery(e.target.value);
+    const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const newQuery = event.target.value;
+        setQuery(newQuery);
+
+        if (searchTimeout.current) {
+            clearTimeout(searchTimeout.current);
+        }
+
+        if (newQuery.trim() !== '') {
+            searchTimeout.current = setTimeout(() => {
+                onSearch(newQuery);
+            }, 500);
+        } else {
+            if (searchTimeout.current) {
+                clearTimeout(searchTimeout.current);
+            }
+        }
     };
 
-    const handleSearch = () => {
-        console.log("Searching for:", query);
-        onSearch(query);
-    };
+    const handleEnterPressed = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') {
+            if (searchTimeout.current) {
+                clearTimeout(searchTimeout.current);
+            }
+            onSearch(query);
+        }
+    }
 
     // Validates whether the result is a subject
     const isSubject = (result: any) => result.type === 'subject';
@@ -49,62 +71,66 @@ const Search: React.FC<SearchProps> = ({ onClose, onSearch, searchQuery, searchR
     }, [onClose])
 
     return (
-        <div className="fixed inset-0 bg-black-100 backdrop-blur-[2px] bg-opacity-25 flex justify-center items-center z-10 transition-all duration-200">
-            <div ref={searchRef} className="bg-white-900 dark:bg-black-100 rounded-2xl w-full max-w-md">
-                <div className="relative flex justify-between items-center border-b border-gray-500">
+        <div className="fixed inset-0 bg-black-500 dark:bg-white-500 bg-opacity-10 dark:bg-opacity-10 backdrop-blur-[2px] flex justify-center items-center z-10 transition-all duration-200">
+            <div ref={searchRef} className="bg-white-900 dark:bg-black-100 rounded-3xl w-full max-w-md">
+                <div className="flex items-center border-b border-gray-500">
+                    <div className="pl-4">
+                        <svg width="16" height="16" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M17 17L12.3846 12.3846M14.3415 7.67077C14.3415 11.3549 11.3549 14.3415 7.67077 14.3415C3.98661 14.3415 1 11.3549 1 7.67077C1 3.9866 3.98661 0.999999 7.67077 0.999999C11.3549 0.999999 14.3415 3.9866 14.3415 7.67077Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                        </svg>
+                    </div>
                     <input
+                        ref={inputRef}
                         type="text"
-                        className="pl-12 py-4 bg-white-900 dark:bg-black-100 w-full rounded-t-2xl outline-none text-xl"
+                        className="pl-4 py-4 bg-white-900 dark:bg-black-100 w-full rounded-t-2xl outline-none text-xl dark:text-white-900 placeholder:text-white-900"
                         value={query}
                         onChange={handleSearchChange}
+                        onKeyDown={handleEnterPressed}
                         placeholder="Search"
                         autoFocus
                     />
-                    <div className="absolute left-4">
-                        <svg width="16" height="16" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M17 17L12.3846 12.3846M14.3415 7.67077C14.3415 11.3549 11.3549 14.3415 7.67077 14.3415C3.98661 14.3415 1 11.3549 1 7.67077C1 3.9866 3.98661 0.999999 7.67077 0.999999C11.3549 0.999999 14.3415 3.9866 14.3415 7.67077Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                        </svg>
-                    </div>
 
-                    <button onClick={onClose} 
-                        className="absolute right-4 px-1 bg-white-700 dark:bg-black-300 rounded-md
-                            border border-white-500 dark:border-black-500 text-black-100 dark:text-white-900"
-                    >
-                        esc
-                    </button>
+                    <div className="flex items-center pr-4">
+                        <button
+                            className="w-8 h-8 flex rounded-full justify-center items-center bg-white-500 hover:bg-white-600 dark:bg-black-500 dark:hover:bg-black-600 active:scale-95 transition-all duration-200"
+                            
+                            onClick={onClose}
+                        >
+                            <svg width="10" height="10" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M17 1L1 17M1 1L17 17" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                            </svg>
+                        </button>
+                    </div>
                 </div>
-                <div className="flex flex-col p-4">
-                    <button onClick={handleSearch} className="w-full p-2 bg-blue-600 text-white rounded hover:bg-blue-700">
-                        Search
-                    </button>
+                <div className="flex flex-col p-4">                    
                     <div className="flex flex-col">
                         {searchResults.length > 0 ? (
-                        <ul>
-                            {searchResults.map((result, index) => (
-                            <li key={result.id || result._id || index} className="flex flex-col gap-4">
-                                <h2 className="text-2xl">
-                                {result.metadata?.semesterName || result.semesterName || "No semester"}
-                                </h2>
-                                {isSubject(result) ? (
-                                <>
-                                    {Array.isArray(result.files) && result.files.length > 0 && (
-                                    <div className="flex flex-col gap-4">
-                                        {result.files.map((file: any) => (
-                                        <SearchFileItem key={file.id || file._id} file={file} /> // Use FileItem component
-                                        ))}
-                                    </div>
+                            <ul>
+                                {searchResults.map((result, index) => (
+                                <li key={result.id || result._id || index} className="flex flex-col gap-4">
+                                    <h2 className="text-2xl">
+                                        {result.metadata?.semesterName || result.semesterName || "No semester"}
+                                    </h2>
+                                    {isSubject(result) ? (
+                                        <>
+                                            {Array.isArray(result.files) && result.files.length > 0 && (
+                                            <div className="flex flex-col gap-4">
+                                                {result.files.map((file: any) => (
+                                                <SearchFileItem key={file.id || file._id} file={file} /> // Use FileItem component
+                                                ))}
+                                            </div>
+                                            )}
+                                        </>
+                                    ) : (
+                                        <>
+                                            <SearchFileItem file={result} /> {/* Use FileItem component for single file */}
+                                        </>
                                     )}
-                                </>
-                                ) : (
-                                <>
-                                    <SearchFileItem file={result} /> {/* Use FileItem component for single file */}
-                                </>
-                                )}
-                            </li>
-                            ))}
-                        </ul>
+                                </li>
+                                ))}
+                            </ul>
                         ) : (
-                        <p className="mt-4 text-gray-500">No results found</p>
+                            <p className="pb-4 text-gray-500">No results found.</p>
                         )}
                     </div>
                 </div>

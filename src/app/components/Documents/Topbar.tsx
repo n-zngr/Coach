56d00f3d69e9b0da314aeba6a7fc543c1462adef
n-zngr <deltagaming.ch.nik@gmail.com>
2025-03-26 13:36:01 +0100

    style(documents): update topbar styling

--- src/app/components/Documents/Topbar.tsx
+++ src/app/components/Documents/Topbar.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 
 /*interface TopbarProps {
     path: string;
@@ -5,21 +6,21 @@
 
 const Topbar: React.FC = () => {
     return (
-        <header
-            className={`
-                relative
-                flex items-center justify-start
-                bg-none
-                border-b border-black-900 dark:border-white-100
-                transition-transform duration-300
-                w-{100%} h-16
-            `}
+        <header className='fixed
+            flex items-center justify-start
+            bg-white-900 dark:bg-black-100
+            border-b border-black-900 dark:border-white-100
+            transition-transform duration-300
+            w-[100%] h-[4.5rem]'
         >
-        <div className="flex items-center h-full px-6">
-            <h1 className="text-xl font-light text-black-900 dark:text-white-100">
-                Documents
-            </h1>
-        </div>
+            <div className="flex items-center h-full px-10">
+                <Link href="/documents" className="bg-none
+                    border border-white-900 hover:border-black-100 dark:border-black-100 dark:hover:border-white-900 rounded-full
+                    text-lg font-light text-black-500 dark:text-white-500
+                    px-2 transition-colors duration-300">
+                    Documents
+                </Link>
+            </div>
         </header>
     )
 }

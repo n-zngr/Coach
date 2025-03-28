fab3fddc91558ae643be96dcf7f7448c75fd0f76
n-zngr <deltagaming.ch.nik@gmail.com>
2025-03-28 22:07:23 +0100

    style(add-button)*: style button ensuring consistency

--- src/app/components/Buttons/AddButton.tsx
+++ src/app/components/Buttons/AddButton.tsx
@@ -5,9 +5,20 @@
 const AddButton: React.FC = () => {
     
     return (
-        <div className="">
-
-        </div>
+        <button className="flex items-center
+        bg-black-100 dark:bg-white-900 hover:bg-white-900 dark:hover:bg-black-100
+        border hover:border-black-100 dark:hover:border-white-900 rounded-full
+        text-white-900 hover:text-black-100 dark:text-black-100 dark:hover:text-white-900
+        gap-2 py-1 px-4 mb-2
+        transition-colors duration-300
+        ">
+            <div className="h-4 w-4 flex justify-center">
+                <svg width="100%" height="100%" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M5 1V9M1 4.97538H9" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"/>
+                </svg>
+            </div>
+            <p>Add</p>
+        </button>
     )
 }
 

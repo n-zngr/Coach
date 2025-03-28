7212f09f2934cbe35713cf484cd3d5351bf26237
n-zngr <deltagaming.ch.nik@gmail.com>
2025-03-28 19:17:01 +0100

    style(top-bar): fix visual bug, button overlapping
    - update z-index ensuring button no longer overlapping on topbar

--- src/app/components/Documents/Topbar.tsx
+++ src/app/components/Documents/Topbar.tsx
@@ -41,11 +41,11 @@ const Topbar: React.FC<TopbarProps> = ({ path }) => {
 
     return (
         <header className='fixed
-        flex items-center justify-start
-        bg-white-900 dark:bg-black-100
-        border-b border-black-900 dark:border-white-100
-        transition-transform duration-300
-        w-[100%] h-[4.5rem]'>
+            flex items-center justify-start
+            bg-white-900 dark:bg-black-100
+            border-b border-black-900 dark:border-white-100
+            transition-transform duration-300
+            w-full h-[4.5rem] z-[1]'>
         <div className="flex items-center h-full px-10">
             <Link href="/documents" className="bg-none
                 border border-white-900 hover:border-black-100 dark:border-black-100 dark:hover:border-white-900 rounded-full

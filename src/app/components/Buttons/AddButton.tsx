ca75e8a2a2f8c4b3f58006bd9f72280783594192
n-zngr <deltagaming.ch.nik@gmail.com>
2025-03-28 11:19:16 +0100

    feat(components): pre-implement component
    - implement components without styling or logic for future documents page implementation

---
+++ src/app/components/Buttons/AddButton.tsx
@@ -0,0 +1,14 @@
+
+/**
+ * Add Button displaying dropdown to add or upload items in documents page.
+*/
+const AddButton: React.FC = () => {
+    
+    return (
+        <div className="">
+
+        </div>
+    )
+}
+
+export default AddButton;

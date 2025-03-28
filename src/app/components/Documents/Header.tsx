29d8b15e6345a74fb5c65939b3b9fdc89264b6e6
n-zngr <deltagaming.ch.nik@gmail.com>
2025-03-28 21:17:55 +0100

    feat(documents): implement header with children
    - implement page displaying header below topbar and above folder list
     - allows children to be passed as simple string
    - implement add-button in header, allowing for further implementation in future

--- src/app/components/Documents/Header.tsx
+++ src/app/components/Documents/Header.tsx
@@ -1,10 +1,17 @@
 import AddButton from "../Buttons/AddButton";
 
-const DocumentsHeader: React.FC = () => {
+interface DocumentsHeaderProps {
+    children: React.ReactNode;
+}
+
+const DocumentsHeader: React.FC<DocumentsHeaderProps> = ({ children }) => {
 
     return (
-        <header>
-            <AddButton></AddButton>
+        <header className="border-b border-black-500 dark:border-white-500 mb-8">
+            <div className="flex justify-between">
+                <h1 className="font-base text-xl self-end pb-1">{children}</h1>
+                <AddButton />
+            </div>
         </header>
     )
 }

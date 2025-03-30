fabb61afd8a460a7ead4a168aef5a17a2c18cbfb
n-zngr <deltagaming.ch.nik@gmail.com>
2025-03-30 19:18:06 +0200

    fix(api): attempt to fix api request

--- src/app/api/documents/delete/[fileId]/route.ts
+++ src/app/api/documents/delete/[fileId]/route.ts
@@ -7,9 +7,9 @@ import { getCollection } from '@/app/utils/mongodb';
 const DATABASE_NAME = 'documents';
 const COLLECTION_NAME = 'fs.files';
 
-export async function DELETE(_request: Request, { params }: { params: { fileId: string } } ) {
+export async function DELETE(request: Request, context: { params: { fileId: string } }) {
     try {
-        const { fileId } = await params;
+        const { fileId } = context.params;
 
         const bucket = await getCollection(DATABASE_NAME, COLLECTION_NAME);
         const deleteFile = await bucket.deleteOne({ _id: new ObjectId(fileId) });

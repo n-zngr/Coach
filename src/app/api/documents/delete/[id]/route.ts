1663acf0780bdd3d698d20982063a7952a2a1b8e
n-zngr <deltagaming.ch.nik@gmail.com>
2025-03-31 10:03:25 +0200

    fix(api): revert delete route from dev-zngr

--- src/app/api/documents/delete/[id]/route.ts
+++ src/app/api/documents/delete/[id]/route.ts
@@ -1,17 +1,15 @@
 import { NextResponse } from 'next/server';
+
 import { ObjectId } from 'mongodb';
 import { getCollection } from '@/app/utils/mongodb';
 
 const DATABASE_NAME = 'documents';
 const COLLECTION_NAME = 'fs.files';
 
-export async function DELETE({ params }: { params: { id: string } } ) {
+export async function DELETE(_req: Request, { params }: { params: { id: string } }) {
+
     try {
         const { id } = await params;
-
-        if (!id) {
-            return NextResponse.json({ message: 'File ID is required' }, { status: 400 });
-        }
 
         const bucket = await getCollection(DATABASE_NAME, COLLECTION_NAME);
         const deleteFile = await bucket.deleteOne({ _id: new ObjectId(id) });
@@ -23,6 +21,6 @@ export async function DELETE({ params }: { params: { id: string } } ) {
         return NextResponse.json({ message: 'File deleted successfully' });
     } catch (error) {
         console.error('Error deleting file:', error);
-        return NextResponse.json({ message: 'Failed to delete file', error: error instanceof Error ? error.message : error }, { status: 500 });
+        return NextResponse.json({ message: 'Failed to delete file', error }, { status: 500 });
     }
 }

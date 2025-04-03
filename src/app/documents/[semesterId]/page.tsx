1e8fe9d3a699d7b1e86cda8d89ca54b64a50410e
n-zngr <deltagaming.ch.nik@gmail.com>
2025-04-03 21:07:31 +0200

    refactor: remove commented-out code
    - remove unused commented-out code
    - update spacing and styling to match /documents

--- src/app/documents/[semesterId]/page.tsx
+++ src/app/documents/[semesterId]/page.tsx
@@ -221,113 +221,21 @@ export default function SemesterPage() {
                 `}
             >
                 <Topbar path={toTitleCase(semesterName)} />
-                <div className='p-12 pt-[7.5rem]'>
-                    <div>
-                        <FolderList
-                            items={subjects}
-                            basePath={`/documents/${semesterId}`}
-                            onRename={handleRenameSubject}
-                            onDelete={handleDeleteSubject}
-                            onAddItem={handleSubmit}
-                            onUploadSuccess={fetchSubjects}
-                            onTriggerUpload={() => setTriggerUpload(true)}
-                            itemType="subjects"
-                        >
-                            {toTitleCase(semesterName) || 'Semester'} Subjects
-                        </FolderList>
-                    </div>
-                    {/*}
-                    <div className="mb-4">
-                        <input
-                            type="text"
-                            className="border rounded p-2 w-full"
-                            placeholder="Subject Name"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                        />
-                        <button
-                            className="bg-blue-500 text-white px-4 py-2 rounded mt-2"
-                            onClick={handleSubmit}
-                        >
-                            Add Subject
-                        </button>
-                    */}
-                        {/* Pass fetchSubjects as a prop to IcsUploader */}
-                    {/*</div>*/}
-
-                    {/*<IcsUploader onUploadSuccess={fetchSubjects} />*/}
-                    {/*}
-                    <h2 className="text-xl font-semibold mb-2">Subjects</h2>
-                    {subjects.length === 0 ? (
-                        <p>No subjects found. Add a new subject to get started.</p>
-                    ) : (
-                        <ul className="flex flex-wrap gap-4 mb-4">
-                            {subjects.map((subject) => (
-                                <div key={subject.id} className="
-                                    flex flex-1 gap-4 p-4
-                                    bg-white dark:bg-neutral-950
-                                    border border-rounded rounded-lg border-neutral-200 dark:border-neutral-800
-                                    hover:bg-neutral-100 hover:dark:bg-neutral-900
-                                    transition-colors duration-300
-                                ">
-                                    <a href={`/documents/${semesterId}/${subject.id}`} className="flex flex-1 gap-4">
-                                        <div className='flex justify-center'>
-                                            <svg className='w-12 h-12' width="19" height="16" viewBox="0 0 19 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                                <path fillRule="evenodd" clipRule="evenodd" d="M0.794996 0.781049C1.30402 0.280952 1.99441 0 2.71429 0H7.54571C7.80668 0 8.04453 0.147036 8.15746 0.378179L9.92698 4H16.2857C17.0056 4 17.696 4.28095 18.205 4.78105C18.714 5.28115 19 5.95942 19 6.66667V13.3333C19 14.0406 18.714 14.7189 18.205 15.219C17.696 15.719 17.0056 16 16.2857 16H2.71429C1.99441 16 1.30402 15.719 0.794996 15.219C0.285968 14.7189 0 14.0406 0 13.3333V2.66667C0 1.95942 0.285969 1.28115 0.794996 0.781049ZM2.71429 1.33333C2.35435 1.33333 2.00915 1.47381 1.75464 1.72386C1.50013 1.97391 1.35714 2.31304 1.35714 2.66667V13.3333C1.35714 13.687 1.50013 14.0261 1.75464 14.2761C2.00915 14.5262 2.35435 14.6667 2.71429 14.6667H16.2857C16.6457 14.6667 16.9908 14.5262 17.2454 14.2761C17.4999 14.0261 17.6429 13.687 17.6429 13.3333V6.66667C17.6429 6.31304 17.4999 5.97391 17.2454 5.72386C16.9908 5.47381 16.6457 5.33333 16.2857 5.33333H9.5C9.23903 5.33333 9.00118 5.1863 8.88825 4.95515L7.11873 1.33333H2.71429Z"/>
-                                            </svg>
-                                        </div>
-                                        {editingSubjectId !== subject.id && (
-                                            <div className='flex flex-col flex-1'>
-                                                <p className='font-bold capitalize'>{subject.name}</p>
-                                            </div>
-                                        )}
-                                    </a>
-                                    {editingSubjectId === subject.id && (
-                                        <div className='flex flex-col flex-1'>
-                                            <input
-                                                type="text"
-                                                value={editingSubjectName}
-                                                onChange={(e) => setEditingSubjectName(e.target.value)}
-                                                className="border rounded p-1"
-                                            />
-                                        </div>
-                                    )}
-                                    */}
-                                    {/*}
-                                    <div className="flex gap-2">
-                                        <button
-                                            onClick={() => handleDeleteSubject(subject.id)}
-                                            className="bg-red-500 text-white px-2 py-1 rounded"
-                                        >
-                                            Delete
-                                        </button>
-                                        {editingSubjectId === subject.id ? (
-                                            <button
-                                                onClick={() => handleEditSubject(subject.id, editingSubjectName)}
-                                                className="bg-green-500 text-white px-2 py-1 rounded"
-                                            >
-                                                Save
-                                            </button>
-                                        ) : (
-                                            <button
-                                                onClick={() => {
-                                                    setEditingSubjectId(subject.id);
-                                                    setEditingSubjectName(subject.name);
-                                                }}
-                                                className="bg-yellow-500 text-white px-2 py-1 rounded"
-                                            >
-                                                Edit
-                                            </button>
-                                        )}
-                                    </div>}
-                                </div>
-                            ))}
-                        </ul>
-                    )}*/}
-                    <h1 className='text-2xl font-semibold my-4'>Documents</h1>
-                    <RecentFiles />
+                <div className='flex flex-col gap-12 p-12 pt-[7.5rem]'>
+                    <FolderList
+                        items={subjects}
+                        basePath={`/documents/${semesterId}`}
+                        onRename={handleRenameSubject}
+                        onDelete={handleDeleteSubject}
+                        onAddItem={handleSubmit}
+                        onUploadSuccess={fetchSubjects}
+                        onTriggerUpload={() => setTriggerUpload(true)}
+                        itemType="subjects"
+                    >
+                        {toTitleCase(semesterName) || 'Semester'} Subjects
+                    </FolderList>
+                    <RecentFiles onFileClick={handleFileClick} />
                     <DisplayFiles onFileClick={handleFileClick} onLinkClick={handleLinkClick} />
-
                 </div>
             </div>
         </div>
